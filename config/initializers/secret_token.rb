9ccac2a7919c7985368c1a4f8767598aa4c705cc
YeonJu Mok <ymok@colgate.edu>
2014-09-02 18:09:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Luxola::Application.config.secret_key_base = '2840db2aaee7df3a18f70ec07169761c00ee98d3f295bfd0fa7386886363047eb529bba96349dae99c96312e5dcd8013a264e2cf92e91be6862defa989a8e977'
