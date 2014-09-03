6d17c83db7e0680508f4dce08282b656d886036d
YeonJu Mok <ymok@colgate.edu>
2014-09-03 09:29:23 +0800

    refactoring

--- app/models/product_list.rb
+++ app/models/product_list.rb
@@ -1,5 +1,6 @@
 class ProductList < ActiveRecord::Base
-	validates :title, presence: true
-	validates :title, length: { minimum: 3}
+	validates :name, presence: true
+	validates :name, length: { minimum: 3}
 	validates :description, length: {minimum: 5}
+	validates :price, numericality: {greater_than_or_equal_to: 0}
 end
